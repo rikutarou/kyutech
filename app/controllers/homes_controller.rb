784b2dcd7a8c260b6d17d1196d0f43f9f8a76083
Tohshi Maeda <p111041t@mail.kyutech.jp>
2017-12-13 12:13:53 +0900

    page order by id

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,7 +2,7 @@ class HomesController < ApplicationController
 
 def index
 if user_signed_in? then
-  @pages = Page.where(user_id: current_user.id)
+  @pages = Page.where(user_id: current_user.id).order(:id)
 #  logger.debug(current_user.inspect)
 #  logger.debug(current_user.id)
 #  logger.debug(@pages.inspect)
