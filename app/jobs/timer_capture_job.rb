8db9872abe26635f80092dd52c41960b629d7b2d
Tohshi Maeda <p111041t@mail.kyutech.jp>
2017-12-13 14:49:29 +0900

    just 0min

--- app/jobs/timer_capture_job.rb
+++ app/jobs/timer_capture_job.rb
@@ -9,6 +9,7 @@ class TimerCaptureJob < ApplicationJob
     # Next schedule
 #    TimerCaptureJob.set(wait: 60.minute).perform_later
     nt = (Time.now+3600)
+    p nt
     TimerCaptureJob.set(run_at: "#{nt.year}-#{nt.month}-#{nt.day} #{nt.hour}:00:00").perform_later
 
   end
